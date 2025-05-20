dd93d3bdfc9825bf23220da12d74c49b6e6f1007
fraumarzhuk <fraumarzhuk@gmail.com>
2025-05-20 15:40:16 +0200

    add socket cpp

---
+++ ASocket.cpp
@@ -0,0 +1,51 @@
+#include "ASocket.hpp"
+
+ASocket::ASocket(int domain, int service, int protocol, int port, u_long interface) {
+	_address.sin_family = domain;
+	_address.sin_port = htons(port);
+	_address.sin_addr.s_addr = htonl(interface);
+
+	_socket = socket(domain, service, protocol);
+	//test connection ?
+	_connection = bind_connection(_socket, _address);
+}
+
+ASocket::ASocket(const ASocket &other) {
+	*this = other;
+}
+
+ASocket &ASocket::operator = (const ASocket &other) {
+	if (this != &other) {
+		//assign;
+	}
+	return (*this);
+}
+
+ASocket::~ASocket() {}
+
+/*getters*/
+int ASocket::get_socket() const {
+	return _socket;
+}
+
+int ASocket::get_connection() const {
+	return _connection;
+}
+
+struct sockaddr_in ASocket::get_address() {
+	return _address;
+}
+
+/*setters*/
+void ASocket::set_socket(int socket) {
+	_socket = socket;
+}
+
+void ASocket::set_connection(int connection) {
+	_connection = connection;
+}
+
+void ASocket::set_address(sockaddr_in address) {
+	_address = address;
+}
+
