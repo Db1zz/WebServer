212e64bd34bf24d5c3728408c93264764a7b6017
8mptyf1ed <96003760+Db1zz@users.noreply.github.com>
2025-08-28 17:04:00 +0200

    Request buffer (#23)
    add data buffer for a client which will be used to store POST request from a client

--- Server/ClientSocket.cpp
+++ Server/ClientSocket.cpp
@@ -1,9 +1,10 @@
 #include "ClientSocket.hpp"
 
-ClientSocket::ClientSocket()
-	: Socket(), _server_fd(-1) {}
+ClientSocket::ClientSocket() : Socket(), _request_ready(false), _server_fd(-1) {
+}
 
-ClientSocket::~ClientSocket() {}
+ClientSocket::~ClientSocket() {
+}
 
 void ClientSocket::set_server_fd(int server_fd) {
 	_server_fd = server_fd;
@@ -11,4 +12,17 @@ void ClientSocket::set_server_fd(int server_fd) {
 
 int ClientSocket::get_server_fd() {
 	return _server_fd;
+}
+
+std::string& ClientSocket::get_data_buffer() {
+	return _request_buffer;
+}
+
+void ClientSocket::reset_request_buffer() {
+	_request_ready = false;
+	_request_buffer.clear();
+}
+
+bool ClientSocket::is_request_ready() {
+	return _request_ready;
 }
