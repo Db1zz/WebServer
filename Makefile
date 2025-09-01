e86854c4264965d29d5d0f74ce5c19780dde36b0
Georgijs Onischenko <xel27@inbox.lv>
2025-09-01 20:26:55 +0200

    temp

--- Makefile
+++ Makefile
@@ -53,14 +53,14 @@ docker_build:
 run: docker_build
 	docker attach webserv
 
-test:
-	. $(VENV_DIR)/bin/activate; python3 ./$(TESTS_DIR)/test.py
-
 install_python_libs:
 	python3 -m venv $(VENV_DIR)
 	./$(VENV_DIR)/bin/pip install --upgrade pip
 	./$(VENV_DIR)/bin/pip install -r $(TESTS_DIR)/requirements.txt
 
-compile_and_test: re install_python_libs test
+prepare_test_env: all install_python_libs
 
-.PHONY: all clean fclean re run docker_build test install_python_libs compile_and_test
+test: prepare_test_env
+	. $(VENV_DIR)/bin/activate; python3 ./$(TESTS_DIR)/test.py
+
+.PHONY: all clean fclean re run docker_build test install_python_libs prepare_test_env
--- Tests/test.py
+++ Tests/test.py
@@ -1,8 +1,43 @@
 import requests
-import re
 import subprocess
+import socket
 from colorama import Fore, Back, Style
 from time import sleep
+from utils import extract_addresses_from_cfg
+
+
+class Webserver:
+	def __init__(self):
+		self.binary_path = "./webserv"
+
+	def _wait_server_to_start(self, args=["./Tests/resources/webserver_test.cfg"]):
+		cfgAdresses = extract_addresses_from_cfg(args[0])
+		for _ in range(50):
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+				result = sock.connect_ex((cfgAdresses[0][0], int(cfgAdresses[0][1])))
+				if result == 0:
+					sock.close()
+					return
+			sleep(0.1)
+		return TimeoutError("Unable to start server")
+
+	def start(self, args=["./Tests/resources/webserver_test.cfg"]):
+		self.process = subprocess.Popen(
+			self.binary_path,
+			stdout=subprocess.DEVNULL,
+			stderr=subprocess.DEVNULL)
+		self._wait_server_to_start(args)
+
+	def stop(self):
+		self.process.terminate()
+
+def startServer(args=[""]):
+	binary_path = "./webserv"
+	process = subprocess.Popen(
+		binary_path,
+		stdout=subprocess.DEVNULL,
+		stderr=subprocess.DEVNULL
+	)
 
 class WebserverTester:
 	def __init__(self):
@@ -25,7 +60,10 @@ class WebserverTester:
 class LoadExistingPage:
 	name = "Load existing page"
 	def test():
+		serv = Webserver()
+		serv.start()
 		r = requests.get("http://127.0.0.1/upload.html")
+		serv.stop()
 		return r.status_code, 200
 
 class LoadNonExistantPage:
@@ -43,9 +81,6 @@ class Load404PageWithBrokenPath:
 class CheckPorts:
 	name = "Check Webserver ports"
 	def test():
-		cfgFile = open("./Tests/webserver_test.cfg")
-		cfgContent = cfgFile.read()
-		cfgPorts = re.findall(r'listen\s+([\d\.]+):(\d+);', cfgContent)
 		expected = []
 		result = []
 		for cfgPort in cfgPorts:
@@ -53,20 +88,12 @@ class CheckPorts:
 			r = requests.get(addr)
 			expected.append(cfgPort + ("200",))
 			result.append(cfgPort + (str(r.status_code),))
-
-		cfgFile.close()
 		return expected, result
 
 if __name__ == "__main__":
-	binary_path = "./webserv"
-	args = [""]
-	process = subprocess.Popen(
-		binary_path,
-		stdout=subprocess.DEVNULL,
-		stderr=subprocess.DEVNULL
-	)
 
-	sleep(2)
+	server = Webserver()
+	server.start()
 
 	tester = WebserverTester()
 
@@ -76,5 +103,3 @@ if __name__ == "__main__":
 	tester.addTestCase(CheckPorts)
 
 	tester.run()
-
-	process.terminate()
