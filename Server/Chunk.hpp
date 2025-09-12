8df5cd96292a9319d0c48e5dde1f0c8a3cc14cb7
fraumarzhuk <fraumarzhuk@gmail.com>
2025-09-12 12:58:45 +0200

    add encoded

--- Server/Chunk.hpp
+++ Server/Chunk.hpp
@@ -2,10 +2,14 @@
 #define CHUNK_HPP
 
 #include <string>
+#include <sstream>
 
 class Chunk {
+public:
+	Chunk::Chunk() {}
+	Chunk::~Chunk(){}
 	static std::string encode(const std::string& data);
-	static decode(const std::string& chunk);
+	static std::string decode(const std::string& chunk);
 };
 
 #endif // CHUNK_HPP
